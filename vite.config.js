1cb4f165a0a4d01395b05d70a622b66fb986c6a9
gleb31415 <gnaumkin213@gmail.com>
2025-06-23 01:36:45 +0300

    fixed preview -229

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactLatex1/', // <-- Add this line (use your repo name)
+  base: '/ReactLatex1/', // <-- Trailing slash, matches your repo name!
 })
+
+"homepage": "https://gleb31415.github.io/ReactLatex1",
